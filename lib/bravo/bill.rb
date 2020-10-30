dac1c01e86ea5e0494b3dfcfd963437042f7bb98
Nicolás Cerrini <nicolas@wecode.io>
2020-10-30 14:48:03 -0300

    Hotfix for very old code that keeps failing

--- lib/bravo/bill.rb
+++ lib/bravo/bill.rb
@@ -225,7 +225,10 @@ module Bravo
                        }.merge!(request_header).merge!(request_detail)
 
       keys, values  = response_hash.to_a.transpose
-      self.response = (defined?(Struct::Response) ? Struct::Response : Struct.new("Response", *keys)).new(*values)
+      
+      #self.response = (defined?(Struct::Response) ? Struct::Response : Struct.new("Response", *keys)).new(*values)
+      # Even if it throws a warning, it avoids some parsing errors.
+      self.response = Struct.new("Response", *keys).new(*values)
     end
   end
 end
